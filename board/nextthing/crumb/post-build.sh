18431c92c0a837db91fcdf32719939ceb37066ec
Jason Abele <jason@nextthing.co>
2016-07-12 14:08:49 -0700

    First pass at crumb

---
+++ board/nextthing/crumb/post-build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+TARGET_DIR=$1
+
+BRANCH=$(git symbolic-ref -q HEAD)
+BRANCH=${BRANCH##refs/heads/chip/}
+BRANCH=${BRANCH##refs/heads/nextthing/*/}
+
+BUILDROOT_GITHASH="$(cat buildroot_githash)"
+BUILDROOT_GITHASH="${BUILDROOT_GITHASH:0:8}"
+
+BUILD="$(cat build)"
+
+cat <<EOF >${TARGET_DIR}/etc/issue
+Welcome to CRUMB Buildroot-${BRANCH} build ${BUILD} rev ${BUILDROOT_GITHASH}
+
+CHIP Buildroot contains various open source software.
+
+The source code can be downloaded from:
+http://opensource.nextthing.co/chip/buildroot/${BRANCH}/${BUILD}/build${BUILD}.tar.gz
+
+EOF
