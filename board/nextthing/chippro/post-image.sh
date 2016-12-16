55a0dd7ea64cc4f8142e32f97f586f637ecda6d6
Alex Kaplan <alex@nextthing.co>
2016-12-16 02:03:58 +0000

    chippro: create symbolic links to nand images in post-image

--- board/nextthing/chippro/post-image.sh
+++ board/nextthing/chippro/post-image.sh
@@ -8,3 +8,8 @@ echo "IMAGES_DIR=${IMAGES_DIR}"
 echo "BUILDROOT_DIR=${BUILDROOT_DIR}"
 
 mk_buildroot_images -N nand_configs/Toshiba-SLC-4G-TC58NVG2S0H.config -d "${IMAGES_DIR}" "${BUILDROOT_DIR}"
+
+ln -sf "${IMAGES_DIR}/spl-40000-1000-100.bin"    "${IMAGES_DIR}/flash-spl.bin"      
+ln -sf "${IMAGES_DIR}/uboot-40000.bin"           "${IMAGES_DIR}/flash-uboot.bin"    
+ln -sf "${IMAGES_DIR}/uboot-env-400000.bin"      "${IMAGES_DIR}/flash-uboot-env.bin"
+ln -sf "${IMAGES_DIR}/chip-40000-1000.ubi.sparse""${IMAGES_DIR}/flash-rootfs.bin"   
