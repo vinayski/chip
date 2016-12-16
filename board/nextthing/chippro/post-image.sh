bdccd55c0a3f5148fee84418072357f328120508
Alex Kaplan <alex@nextthing.co>
2016-12-16 06:13:48 +0000

    chippro: use relative paths for flash images in post-image.sh

--- board/nextthing/chippro/post-image.sh
+++ board/nextthing/chippro/post-image.sh
@@ -9,7 +9,9 @@ echo "BUILDROOT_DIR=${BUILDROOT_DIR}"
 
 mk_buildroot_images -N nand_configs/Toshiba-SLC-4G-TC58NVG2S0H.config -d "${IMAGES_DIR}" "${BUILDROOT_DIR}"
 
-ln -sf "${IMAGES_DIR}/spl-40000-1000-100.bin"    "${IMAGES_DIR}/flash-spl.bin"      
-ln -sf "${IMAGES_DIR}/uboot-40000.bin"           "${IMAGES_DIR}/flash-uboot.bin"    
-ln -sf "${IMAGES_DIR}/uboot-env-400000.bin"      "${IMAGES_DIR}/flash-uboot-env.bin"
-ln -sf "${IMAGES_DIR}/chip-40000-1000.ubi.sparse""${IMAGES_DIR}/flash-rootfs.bin"   
+pushd "${IMAGES_DIR}"
+ln -sf "spl-40000-1000-100.bin"     "flash-spl.bin"
+ln -sf "uboot-40000.bin"            "flash-uboot.bin"
+ln -sf "uboot-env-400000.bin"       "flash-uboot-env.bin"
+ln -sf "chip-40000-1000.ubi.sparse" "flash-rootfs.bin"
+popd
